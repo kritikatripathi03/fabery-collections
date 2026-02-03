1b1c9d165bf3ae0de11d38a230b0917a65fc6f02
Kritika Tripathi <kritika.tripathi03@gmail.com>
2026-02-03 22:32:49 +0530

    Basic Navbar changes done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import Navbar from "./components/Navbar";
+
 export default function App() {
   return (
-    <div className="bg-red-500 text-white text-4xl p-10">
-      TAILWIND TEST
+    <div>
+      <Navbar></Navbar>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,48 @@
 export default function Navbar() {
   return (
     <nav>
-<div className="flex gap-20 bg-yellow-200 p-4">
-  <span className="bg-red-500 text-white px-2">One</span>
-  <span className="bg-blue-500 text-white px-2">Two</span>
-  <span className="bg-green-500 text-white px-2">Three</span>
-</div>
+      <div className="flex items-center justify-between px-8 py-4">
+        <div className="flex gap-6">
+          <span className="px-2 font-bold">Home</span>
+          <span className="px-2 font-bold">Collections</span>
+          <span className="px-2 font-bold">New</span>
+        </div>
+
+        <div className="flex gap-6">
+          <img
+            className="h-16 w-16"
+            src="src\assets\logo-small.png"
+            alt="FABERY"
+          />
+          <img
+            className="py-2h-10 w-26"
+            src="src\assets\fabery.png"
+            alt="FABERY"
+          />
+        </div>
+
+        <div className="flex items-center gap-4">
+          <img
+            className="h-12 w-12"
+            src="src\assets\wishlist-icon.png"
+            alt="WISHLIST"
+          />
+          <div className="relative flex items-center ml-10 mr-10">
+            <button className="bg-black text-white px-6 py-3 rounded-full pr-8">
+                Cart
+            </button>
+            <div className="absolute right-0 translate-x-1/2 bg-white border-4 border-black rounded-full h-10 w-10 flex items-center justify-center">
+                <img className="h-5 w-5" src="src\assets\cart.png" alt="WISHLIST" />
+            </div>
+          </div>
+          
+          <img
+            className="h-12 w-12"
+            src="src\assets\profile.png"
+            alt="WISHLIST"
+          />
+        </div>
+      </div>
     </nav>
   );
 }
