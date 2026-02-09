7ce59dc0c9f524222fa7981d6a077a44ca54a754
Kritika Tripathi <kritika.tripathi03@gmail.com>
2026-02-09 21:23:48 +0530

    Special Collection Inserted

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Categories from "./components/Categories";
 import Navbar from "./components/Navbar";
+import SpecialCollection from "./components/SpecialCollection";
 
 export default function App() {
   return (
@@ -7,6 +8,7 @@ export default function App() {
 
       <Navbar></Navbar>
       <Categories></Categories>
+      <SpecialCollection></SpecialCollection>
     </div>
   );
 }
---
+++ src/components/SpecialCollection.jsx
@@ -0,0 +1,7 @@
+export default function SpecialCollection () {
+    return (
+        <div>
+            
+        </div>
+    )
+}
