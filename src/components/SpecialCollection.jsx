3c880084d85d7db24fba2f3b797251e5f6df48f9
Kritika Tripathi <kritika.tripathi03@gmail.com>
2026-02-10 22:18:56 +0530

    led basic layout for New Collection container

--- src/components/SpecialCollection.jsx
+++ src/components/SpecialCollection.jsx
@@ -1,7 +1,26 @@
 export default function SpecialCollection () {
     return (
-        <div>
-            
+        <div className="flex items-center justify-between px-4 py-4">
+            <div className="flex flex-col gap-1 text-sm tracking-widest mb-4">
+                <div>
+                    NEW COLLECTION
+                </div>
+                <div>
+                    Summer 2024
+                </div>
+                <div>
+                    Go To Shop
+                </div>
+            </div>
+            <div className="max-w-[360px] aspect-[3/4]">
+                <img src="src/assets/oversized-tshirt.jpg" className="w-full object-contain"/>
+            </div>
+            <div className="max-w-[360px] aspect-[3/4]">
+                <img src="src/assets/jeans.jpg" className="w-full object-contain"/>
+            </div>
+            <div className="max-w-[360px] aspect-[3/4]">
+                <img src="src/assets/jeans.jpg" className="w-full object-contain"/>
+            </div>
         </div>
     )
 }
