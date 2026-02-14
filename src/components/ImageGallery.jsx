28d205af9413aab2900d4b308282239aab1149d6
Kritika Tripathi <kritika.tripathi03@gmail.com>
2026-02-14 21:55:32 +0530

    Image Gallery Design

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -1,7 +1,32 @@
 export default function ImageGallery() {
-    return (
-        <div>
-            Image Gallery
+  return (
+    <div className="w-full flex justify-between items-start gap-10 py-20">
+      {/* Image 1 */}
+      <div className="flex-1">
+        <div className="aspect-[3/4] overflow-hidden">
+          <img src="src/assets/gal-1.jpg" className="w-full object-cover" />
         </div>
-    )
+      </div>
+
+      {/* Image 2 (shift down) */}
+      <div className="flex-1 mt-20">
+        <div className="aspect-[3/4] overflow-hidden">
+          <img src="src/assets/gal-2.jpg" className="w-full object-cover" />
+        </div>
+      </div>
+      {/* Image 3 */}
+      <div className="flex-1">
+        <div className="aspect-[3/4] overflow-hidden">
+          <img src="src/assets/gal-3.jpg" className="w-full object-cover" />
+        </div>
+      </div>
+
+      {/* Image 4 (shift down) */}
+      <div className="flex-1 mt-20">
+        <div className="aspect-[3/4] overflow-hidden">
+          <img src="src/assets/gal-4.jpg" className="w-full object-cover" />
+        </div>
+      </div>
+    </div>
+  );
 }
