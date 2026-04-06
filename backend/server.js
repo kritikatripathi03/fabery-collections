01fa64d32ceb2b06a89ccf7e7082fe49236303e9
Kritika Tripathi <kritika.tripathi03@gmail.com>
2026-04-06 22:18:57 +0530

    User Auth and JWT Token Generation

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
 const connectDB = require('./config/db')
+const authRoutes = require('./routes/auth')
 
 const app = express()
 
@@ -9,6 +10,8 @@ connectDB()
 
 app.use(cors())
 app.use(express.json())
+
+app.use('/api/auth', authRoutes)
 
 app.get('/', (req, res) => {
     res.send('API is running...')
