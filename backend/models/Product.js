8545dbbbaa77d95e57445db52d7fbd8346b4ccad
Kritika Tripathi <kritika.tripathi03@gmail.com>
2026-04-08 21:52:35 +0530

    Created Product model

---
+++ backend/models/Product.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+      enum: ["men", "women", "kids", "accessories"],
+    },
+    size: {
+      type: [String],
+      enum: ["XS", "S", "M", "L", "XL", "XXL"],
+      default: [],
+    },
+    images: {
+      type: [String],
+      default: [],
+    },
+    stock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    rating: {
+      type: Number,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+module.exports = mongoose.model("Product", productSchema);
