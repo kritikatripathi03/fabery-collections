e824c17414d4b6acb299acfc8b85f3988b1c8117
Kritika Tripathi <kritika.tripathi03@gmail.com>
2026-04-09 21:50:54 +0530

    created Product route

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -3,3 +3,36 @@ const router = express.Router()
 const Product = require('../models/Product')
 
 // Get all products
+router.get('/', async(req, res) => {
+    try {
+        const products = await Product.find()
+        res.json(products)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+//Get single product
+router.get('/:id', async(req, res) => {
+    try {
+        const product = await Product.findById(req.params.id)
+        if(!product) {
+            return res.status(400).json({ message: 'Product not found' })
+        }
+        res.json(product)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+// Get products by category
+router.get('/category/:category', async (req, res) => {
+    try {
+        const products = await Product.find({ category: req.params.category })
+        res.json(products)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+module.exports = router
