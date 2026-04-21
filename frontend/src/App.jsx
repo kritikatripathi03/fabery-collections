0275ceb09a5c69675bb646c9fa1e702326b6967f
Kritika Tripathi <kritika.tripathi03@gmail.com>
2026-04-21 21:57:11 +0530

    Added Login and Register Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,20 +10,27 @@ import Cart from "./components/Cart";
 import Wishlist from "./components/Wishlist";
 import About from "./components/About";
 import Footer from "./components/Footer";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 export default function App() {
   return (
     <Router>
       <Navbar></Navbar>
       <Routes>
+        {/* Public routes */}
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
+        <Route path="/product/:id" element={<ProductProfile />} />
         <Route path="/new" element={<New />} />
-        <Route path="/product" element={<ProductProfile />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        {/* Private routes */}
         <Route path="/profile" element={<Profile />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/wishlist" element={<Wishlist />} />
-        <Route path="/about" element={<About />} />
         
       </Routes>
       <Footer></Footer>
